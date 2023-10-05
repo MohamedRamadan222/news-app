da51c9a98e86e2042c29c1894370e23a0f6bc128
MohamedRamadan222 <khaledabozead575@gmail.com>
2023-10-05 07:04:29 +0300

    Add Search

--- lib/modules/web_view/web_view_screen.dart
+++ lib/modules/web_view/web_view_screen.dart
@@ -1,16 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-class WebViewScreen extends StatelessWidget {
-  final  controller = WebViewController()
-  ..setJavaScriptMode(javaScriptMode.disabled)
-  ..loadRequest("uri");
- // String filePath = 'assets/index.html';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: WebViewWidget(controller: controller)
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class WebViewScreen extends StatelessWidget {
+//   // const WebViewScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: WebView,
+//     );
+//   }
+// }
